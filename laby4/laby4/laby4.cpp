416c0043dd95b6e03938269cab7526746fdadd1b
Struskawka <indi.15.15@gmail.com>
2023-01-03 08:32:53 +0000

    z1

---
+++ laby4/laby4/laby4.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+using namespace std;
+
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+int main()
+{
+try{
+    double n;
+    cin >> n;
+    throw n;
+
+    int m;
+    cin >> m;
+    throw m;
+
+}
+catch (int val) { cout <<val; }
+catch (...) { cout << "wyjatek"; }
+
+    return 0;
+}
